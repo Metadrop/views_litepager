1bef60c6ecc71558d59d9681588db703d61897ef
Eduardo Morales Alberti <12516-EduardoMoralesAlberti@users.noreply.drupalcode.org>
2022-11-04 13:34:24 +0100

    Issue #3318863: Port module to be compatible with Drupal 8, 9 and 10

--- src/Plugin/views/pager/PagerLite.php
+++ src/Plugin/views/pager/PagerLite.php
@@ -20,6 +20,13 @@ use Drupal\views\Plugin\views\pager\Full;
 class PagerLite extends Full {
 
   /**
+   * Indicates if the view has next paage.
+   *
+   * @var bool
+   */
+  protected $next_page = TRUE;
+
+  /**
    * {@inheritdoc}
    */
   public function summaryTitle() {
@@ -56,49 +63,86 @@ class PagerLite extends Full {
   public function query() {
     parent::query();
 
-    $next_page = TRUE;
+    // If option total pages is set, and it gets to the last page, then
+    // next page does not exist.
     if (!empty($this->options['total_pages'])) {
       if (($this->getCurrentPage() + 1) >= $this->options['total_pages']) {
-        $next_page = FALSE;
+        $this->next_page = FALSE;
       }
     }
-    if ($next_page) {
+
+    // Set limit to one more item of the default items per page to know
+    // if there are more items to display in the next page.
+    if ($this->next_page) {
       $limit = $this->getItemsPerPage();
       $this->view->query->setLimit($limit + 1);
     }
   }
 
   /**
-   * {@inheritdoc}
+   * Total items based on current page.
+   *
+   * Always return that there are more items that the current page and
+   * calculate if there are next page after the views execution.
+   *
+   * @return float|int
+   * Total items.
+   */
+  public function getTotalItems() {
+    return $this->getCurrentPage() * $this->getItemsPerPage() + 1;
+  }
+
+  /**
+   * Calculate if there are next page.
+   *
+   * @param array $result
+   *   List of items after run the views query,
+   *
+   * @return void
    */
   public function preRender(&$result) {
-    if (count($result) > $this->options['items_per_page']) {
+
+    // If the items on the result are more than the items per page,
+    // then it should exist a next page.
+    if (count($result) > $this->getItemsPerPage()) {
+      // Remove the item to know if there are next page.
       array_pop($result);
+
       $this->next_page = TRUE;
+      // If the page was not set, then set current page.
       if ($this->getCurrentPage() == -1) {
         $this->setCurrentPage();
       }
+      // Set the next page.
       $this->setCurrentPage($this->getCurrentPage() + 1);
+
     }
     else {
+      // If there are no next page, set current page to 0
+      // or the current page id.
+      $this->next_page = FALSE;
       $this->setCurrentPage();
     }
   }
 
   /**
-   * {@inheritdoc}
+   * Render the pager lite.
+   *
+   * @param array $input
+   *   Views input.
+   *
+   * @return array
+   *   Render array pager lite.
    */
   public function render($input) {
 
-    if (empty($this->getCurrentPage()) && (!isset($this->next_page) || $this->next_page != TRUE)) {
-      return '';
-    }
     return [
       '#theme' => $this->themeFunctions(),
       '#tags' => $this->options['tags'],
       '#element' => $this->options['id'],
       '#parameters' => $input,
       '#route_name' => !empty($this->view->live_preview) ? '<current>' : '<none>',
+      '#has_next' => $this->next_page,
     ];
   }
 
@@ -107,7 +151,7 @@ class PagerLite extends Full {
    */
   public function executeCountQuery(&$count_query) {
     // Do not execute count query to avoid timeout
-    // because of revision table dimensions.
+    // due the revision table dimensions.
   }
 
 }
